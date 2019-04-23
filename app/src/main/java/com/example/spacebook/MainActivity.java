3fa3405c0a5d0da7faf7abc9e7d2a7e197bb9978
“Monica <levy_moni@bentley.edu>
2019-04-23 19:12:21 -0400

    Tab Page
    Signed-off-by: “Monica <levy_moni@bentley.edu>

--- app/src/main/java/com/example/spacebook/MainActivity.java
+++ app/src/main/java/com/example/spacebook/MainActivity.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String test = "YO";
+
     }
 
-    HELLOOOOOOOOO!!!!!!!!!!!!!!!!!!!!!!!!
+
 }
---
+++ app/src/main/java/com/example/spacebook/TabPage.java
@@ -0,0 +1,47 @@
+package com.example.spacebook;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TabHost;
+
+
+public class TabPage extends FragmentActivity implements AdapterView.OnItemSelectedListener  {
+
+    TabHost tabs;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        tabs = findViewById(R.id.tabhost);
+        tabs.setup();
+
+        TabHost.TabSpec spec;
+
+
+        // Initialize a TabSpec for tab1 and add it to the TabHost
+        spec = tabs.newTabSpec("tag1");    //create new tab specification
+        spec.setContent(R.id.My_Reservations);    //add tab view content
+        spec.setIndicator("My Reservations");    //put text on tab
+        tabs.addTab(spec);             //put tab in TabHost container
+
+
+        // Initialize a TabSpec for tab2 and add it to the TabHost
+        spec = tabs.newTabSpec("tag2");        //create new tab specification
+        spec.setContent(R.id.Make_Reservations);            //add view tab content
+        spec.setIndicator("Make Reservations");
+        tabs.addTab(spec);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
