30a53bf8daead3c9c374280a8c56e2bf26bff084
“Joe <deploy_joe@bentley.edu>
2019-04-28 14:22:53 -0400

    login screen design v1

--- app/src/main/java/com/example/spacebook/MainActivity.java
+++ app/src/main/java/com/example/spacebook/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private Cursor cursor;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
