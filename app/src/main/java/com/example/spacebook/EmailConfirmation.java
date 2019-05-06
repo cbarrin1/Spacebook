ab92543ed3ee8908fc640b98ae0bcc026b6e48eb
Maksim Sviridenko <sviride_maks@bentley.edu>
2019-05-06 17:07:53 -0400

    Email confirmation up and running

--- app/src/main/java/com/example/spacebook/EmailConfirmation.java
+++ app/src/main/java/com/example/spacebook/EmailConfirmation.java
@@ -1,52 +1,65 @@
 package com.example.spacebook;
 
+import android.app.TabActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import java.util.ArrayList;
+import java.util.List;
 
+// This class is responsible for sending out confirmation emails to recipients
 public class EmailConfirmation extends AppCompatActivity {
 
-    private EditText email1, email2, email3, email4, email5;
-    String emailText1, emailText2, emailText3, emailText4, emailText5;
-    Button send, noSend;
+    // list for emails
+    List<EditText> emails;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.enter_emails);
+        // initialize array and add(find) views
+        emails = new ArrayList<>();
+        emails.add((EditText) findViewById(R.id.email1));
+        emails.add((EditText) findViewById(R.id.email2));
+        emails.add((EditText) findViewById(R.id.email3));
+        emails.add((EditText) findViewById(R.id.email4));
+        emails.add((EditText) findViewById(R.id.email5));
 
-        email1 = findViewById(R.id.editText);
-        email2 = findViewById(R.id.editText3);
-        email3 = findViewById(R.id.editText4);
-        email4 = findViewById(R.id.editText5);
-        email5 = findViewById(R.id.editText6);
-
-        send = findViewById(R.id.button3);
-        noSend = findViewById(R.id.button4);
-
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // saving any entered email addresses
-                emailText1 = email1.getText().toString();
-                emailText2 = email2.getText().toString();
-                emailText3 = email3.getText().toString();
-                emailText4 = email4.getText().toString();
-                emailText5 = email5.getText().toString();
-
-            }
-        });
-
-        noSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-
-            }
-        });
 
     }
+    // Sends emails when button is clicked
+    public void sendEmails (View view) {
+        // make a string to put all the recepient emails
+        String fullRecepients = "";
+        // get all the values that are not empty from the list
+      for (int i = 0; i<emails.size(); i++){
+            if(!emails.get(i).getText().toString().isEmpty()) {
+                // append the recipients string
+                fullRecepients+= emails.get(i).getText().toString()+ ",";
+            }
+        }
+        // opens gmail and parses recipients addresses, subject and text
+        Intent intent = new Intent(Intent.ACTION_SENDTO,Uri.parse("mailto:" +fullRecepients));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Study room reservation");
+        intent.putExtra(Intent.EXTRA_TEXT, "You have reserved a room");
+        if (intent.resolveActivity(getPackageManager())!=null) {
+            startActivity(intent);
+        }
+        // finishes when done
+        finish();
+
+
+    }
+
+    // transfers directly to my reservations page
+    public void doNotSend (View view) {
+        Intent intent =new Intent(EmailConfirmation.this, TabPage.class);
+        startActivity(intent);
+    }
+
+
 }
